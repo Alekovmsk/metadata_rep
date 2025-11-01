26a14c7bab5a8c89d1e8381b3b4fb43f4c848d14
Байрамуков Назим <nazim.bayramukov@gazprombank.ru>
2025-11-01 16:49:56 +0300

    - добавить в конфиги название таблицы для логирование

--- src/main/java/com/gpb/replication/postgres/logrepository/LogRepository.java
+++ src/main/java/com/gpb/replication/postgres/logrepository/LogRepository.java
@@ -1,7 +1,9 @@
 package com.gpb.replication.postgres.logrepository;
 
+import com.gpb.replication.postgres.properties.LogsDatabaseProperties;
 import com.gpb.replication.postgres.service.CefLogFileService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import java.sql.Timestamp;
@@ -9,45 +11,49 @@ import java.sql.Timestamp;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class LogRepository {
 
     private final JdbcTemplate logsJdbcTemplate;
     private final CefLogFileService cefLogFileService;
+    private final LogsDatabaseProperties logsDatabaseProperties;
+
+
     public Log findLatestByType(String type, String host) {
-        String sql = """
-            SELECT l.id, l.type, l.log, l.created
-            FROM audit.jdata_replicator_log l
-            WHERE l.type = ?
-              AND l.log LIKE ?
-            ORDER BY l.created DESC
-            LIMIT 1
-        """;
+        String table = logsDatabaseProperties.getTable().trim();
+        String sql = String.format("""
+        SELECT l.id, l.type, l.log, l.created
+        FROM %s l
+        WHERE l.type = ?
+          AND l.log LIKE ?
+        ORDER BY l.created DESC
+        LIMIT 1
+        """, table);
 
         return logsJdbcTemplate.query(sql, new Object[]{type, "%" + host + "%"}, rs -> {
             if (rs.next()) {
-                Log log = new Log();
-                log.setId(rs.getObject("id", Integer.class));
-                log.setType(rs.getString("type"));
-                log.setLog(rs.getString("log"));
-                log.setCreated(rs.getTimestamp("created").toLocalDateTime());
-                return log;
+                Log logEntity = new Log();
+                logEntity.setId(rs.getObject("id", Integer.class));
+                logEntity.setType(rs.getString("type"));
+                logEntity.setLog(rs.getString("log"));
+                logEntity.setCreated(rs.getTimestamp("created").toLocalDateTime());
+                return logEntity;
             }
             return null;
         });
     }
-
-    public void save(Log log) {
-        String sql = """
-            INSERT INTO audit.jdata_replicator_log (created, log, type)
-            VALUES (?, ?, ?)
-        """;
+    public void save(Log logEntity) {
+        String table = logsDatabaseProperties.getTable().trim();
+        String sql = String.format("""
+        INSERT INTO %s (created, log, type)
+        VALUES (?, ?, ?)
+        """, table);
 
         logsJdbcTemplate.update(sql,
-                Timestamp.valueOf(log.getCreated()),
-                log.getLog(),
-                log.getType());
+                Timestamp.valueOf(logEntity.getCreated()),
+                logEntity.getLog(),
+                logEntity.getType());
 
-        cefLogFileService.writeToFile(log.getCreated(), log.getLog());
+        cefLogFileService.writeToFile(logEntity.getCreated(), logEntity.getLog());
         cefLogFileService.cleanupOldLogs();
-    }
-}
+    }}
